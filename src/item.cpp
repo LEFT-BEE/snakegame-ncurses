527639f08ef5edb817218edf4f9fbcb27165defb
LEFT-BEE <gkstmdwls99@naver.com>
2021-06-14 14:37:55 +0900

    2021.06.14v2

---
+++ src/item.cpp
@@ -0,0 +1,149 @@
+#include"item.h"
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+extern MapManager *mapManager;
+bool gateflag=false;
+
+CharPosition item::getrnadpos(){
+
+        CharPosition *pos = new CharPosition();
+
+        while(1){
+            int x = rand() % WIDTH;
+            int y = rand() % HEIGHT;
+
+            if(mapManager->data[y][x] == '0'){
+                pos->x = x;
+                pos->y = y;
+                return *pos;
+            }
+        }
+}
+
+itemManager::itemManager()
+{   lastDropTime =0;
+    getmaxyx(stdscr, maxheight, maxwidth);
+}
+
+
+
+
+bool itemManager::istimeover(float eTime){
+  if(eTime - lastDropTime > 5)
+    return TRUE;
+  return false;
+}
+bool itemManager::gatetimeover(float eTime){
+  if(eTime - lastGateTime > 10)
+    return TRUE;
+  return false;
+}
+
+void itemManager::deleteitem(int y , int x){
+  int target;
+  for(int i=0 ; i < data.size(); i++ ){
+    if(data[i].position.x == x && data[i].position.y == y){
+      target = i;
+      data.erase(data.begin() + target);
+    }
+  }
+}
+
+void itemManager::itemtype(string type , float eTime){
+  if(type == "poison"){
+    data.push_back(item("poison"));
+  }
+  else if(type == "gift"){
+    data.push_back(item("gift"));
+  }
+  else{
+    data.push_back(item("biggift"));
+  }
+}
+
+
+void itemManager::pushdata(){
+    for(int i=0; i < data.size();i++){
+        if(data[i].type == "poison"){
+          mapManager->PatchData(data[i].position.y , data[i].position.x , '5');
+        }
+        else if(data[i].type == "gift"){
+          mapManager->PatchData(data[i].position.y , data[i].position.x , '6');
+        }
+        else if(data[i].type == "biggift"){
+          mapManager->PatchData(data[i].position.y , data[i].position.x , '7');
+        }
+    }
+}
+
+
+void itemManager::Update(float eTime){
+
+    if(istimeover(eTime)){
+      for(int i=0 ; i < data.size(); i++ ){
+        mapManager-> PatchData(data[i].position.y , data[i].position.x , '0');
+      }
+      data.clear();
+      for(int i=0; i < 3; i++){
+        int r = rand() % 3;
+        if(r == 0)
+          itemtype("poison" , eTime);
+        else if(r == 1)
+          itemtype("gift" , eTime);
+        else{
+          itemtype("biggift" , eTime);
+        }
+
+      }
+      pushdata();
+      lastDropTime = eTime;
+    }
+
+    if(gatetimeover(eTime)){
+      if(!gateflag)
+      {
+        DeleteGate();
+        MakeGate();
+        lastGateTime = eTime;
+      }
+    }
+}
+
+struct Position Gate1={0,};
+struct Position Gate2={0,};
+int Gatecnt=0;
+
+void itemManager::MakeGate()
+{
+  if(Gatecnt==0)
+  {
+    Gate1.y=rand()%HEIGHT;
+    if(Gate1.y>(HEIGHT/2)) Gate1.y=HEIGHT-2;
+    else Gate1.y=1;
+    Gate1.x=rand()%(WIDTH-3)+2;
+
+    Gate2.y=rand()%(HEIGHT-3)+2;
+    Gate2.x=rand()%WIDTH;
+    if(Gate2.x>(WIDTH/2)) Gate2.x=WIDTH-2;
+    else Gate2.x=1;
+
+    mapManager->PatchData(Gate1.y ,Gate1.x , '8');
+    Gatecnt++;
+    mapManager->PatchData(Gate2.y ,Gate2.x , '8');
+    Gatecnt++;
+  }
+}
+
+void itemManager::DeleteGate()
+{
+  if(Gatecnt==2)
+  {
+    mapManager->PatchData(Gate1.y ,Gate1.x , '1');
+    Gatecnt--;
+
+    mapManager->PatchData(Gate2.y ,Gate2.x , '1');
+    Gatecnt--;
+  }
+}
---
+++ src/item.h
@@ -0,0 +1,55 @@
+#include <vector>
+#include <cstdlib>
+#include "make_map.h"
+#include "IObject.h"
+#include "CharPosition.h"
+#include <ncurses.h>
+#include <string>
+#include <vector>
+#include<ctime>
+
+struct Position{
+  int y;
+  int x;
+};
+class item:public IObject
+{
+public:
+  string type = "none";
+  CharPosition position;
+  CharPosition getrnadpos();
+  item(string type_):type(type_)
+    {
+      CharPosition temp = getrnadpos();
+      position.x = temp.x;
+      position.y = temp.y;
+    }
+  ~item(){};
+  void Update(float eTime){};
+  void Render(){};
+};
+
+
+class itemManager:public IObject
+{
+
+public:
+    int maxheight, maxwidth;
+    vector<item> data;
+    float lastDropTime;
+    float lastGateTime;
+    itemManager();
+    ~itemManager(){};
+    void Update(float eTime);
+    void Render(){};
+    bool istimeover(float eTIme);
+    bool gatetimeover(float eTime);
+    void pushdata();
+    void itemtype(std::string check, float eTime);
+    void deleteitem(int y, int x);
+    void MakeGate();
+    void DeleteGate();
+
+
+    float start_time , end_time;
+};
